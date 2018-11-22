f5719833154a256bf3a1f52afb8f28d8e9861402
marianapugliese <mpugliesem@gmail.com>
2018-11-22 18:06:25 -0300

    hola

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*a+51(9yv()44xa!=bl4!7akxa-v$#dmq6o9s&7a16c*bwg)3e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'marianapugliese.pythonanywhere.com']
 
 
 # Application definition
